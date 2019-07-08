bd96c42e8229af652dd9f308f95fd36caff38a1f
Esteban Hernandez <contact@mksmdesign.com>
2019-07-08 15:08:43 -0600

    Adding in Redux functionality

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import store, {ADD_INSTRUCTION} from "../../store";
 
 class Instructions extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState();
     this.state = {
-      instructions: [],
+      instructions: reduxState.instructions,
       input: ""
     };
   }
@@ -15,7 +17,10 @@ class Instructions extends Component {
     });
   }
   addInstruction() {
-    // Send data to Redux state
+    store.dispatch({
+      type: ADD_INSTRUCTION,
+      payload: this.state.input
+    });
     this.setState({
       input: ""
     });
